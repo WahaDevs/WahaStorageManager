be754fee7a53eba44661e9e2f442116c2ae3bdbc
Wylarel <timjemine@gmail.com>
2020-08-21 12:41:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
-cred = credentials.Certificate('./ServiceAccountKey.json')
+cred = credentials.Certificate('ServiceAccountKey.json')
 firebase_app = firebase_admin.initialize_app(cred, {
     'storageBucket': 'wahaapplication.appspot.com'
 })
